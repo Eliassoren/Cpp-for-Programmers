e4db8a4d5de2f5cea8e718bf977e4f78704033e5
Elias <eliassorr@gmail.com>
2017-11-24 21:40:54 +0100

    Fixed bind to set.cpp.

--- oving07/set/set.cpp
+++ oving07/set/set.cpp
@@ -1,46 +1,46 @@
-// #include "set.hpp"
-// #include <set>
-// using namespace std;
+#include "set.hpp"
 
+// Empty set
+Set::Set() {
+  entries = {
 
-// Set::Set() {
-//   entries = {
+  };
+}
 
-//   };
-// }
+Set::Set(std::vector<int> vec) : entries(vec) {
+}
 
-// Set::Set(std::vector<int> vec) {
-//   entries = vec;
-// }
+// Union of two sets
+Set Set::operator+(Set &otherSet) const {
+  std::set<int> unionEntries;
+  for (auto &val : entries) {
+    // Add element if search is unsuccessful. No duplicates
+    if (unionEntries.find(val) == unionEntries.end()) {
+      unionEntries.insert(val);
+    }
+  }
+  for (auto &val : otherSet.getEntries()) {
+    // Add element if search for element was unsuccessful. No duplicates
+    if (unionEntries.find(val) == unionEntries.end()) {
+      unionEntries.insert(val);
+    }
+  }
+  vector<int> newEntries(unionEntries.begin(), unionEntries.end());
+  Set newSet(newEntries);
+  return newSet;
+}
 
+// Add element
+Set Set::operator+(int &val) const {
 
-// Set Set::operator+(Set &otherSet) const {
-//   std::set<int> unionSet;
-//   for (auto &val : entries) {
-//     if (unionSet.find(val) == unionSet.end()) {
-//       unionSet.insert(val);
-//     }
-//   }
-//   for (auto &val : otherSet.getEntries()) {
-//     // Search for element was unsuccessful
-//     if (unionSet.find(val) == unionSet.end()) {
-//       unionSet.insert(val);
-//     }
-//   }
-//   vector<int> newEntries(unionSet.begin(), unionSet.end());
-//   Set newSet(newEntries);
-//   return newSet;
-// }
-// Set Set::operator+(int &val) const {
-
-//   std::set<int> values(entries.begin(), entries.end());
-//   if (values.find(val) == values.end())
-//     values.insert(val);
-//   vector<int> newEntries(values.begin(), values.end());
-//   Set set(newEntries);
-//   return set;
-// }
-
-// void Set::operator=(Set &set) {
-//   entries = set.getEntries();
-// }
+  std::set<int> values(entries.begin(), entries.end());
+  if (values.find(val) == values.end())
+    values.insert(val);
+  vector<int> newEntries(values.begin(), values.end());
+  Set set(newEntries);
+  return set;
+}
+// Assign set to other set.
+void Set::operator=(Set &set) {
+  entries = set.getEntries();
+}
